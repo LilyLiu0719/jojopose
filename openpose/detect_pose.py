6086bebf0627d1526eebadbb9690f0c55212cfdc
LilyLiu0719 <lily.ylliu@gmail.com>
2021-01-15 08:57:06 +0000

    add openpose

---
+++ openpose/detect_pose.py
@@ -0,0 +1,20 @@
+import pyopenpose as op
+
+class PoseDetector():
+    def __init__(self, model_path="./models/"):
+        # load models 
+        params = dict()
+        params["model_folder"] = model_path
+        print("[*] success loading model from", model_path)
+
+        # Starting OpenPose
+        self.opWrapper = op.WrapperPython()
+        self.opWrapper.configure(params)
+        self.opWrapper.start()
+        print("[*] openpose start!")
+
+    def processImage(self, imageToProcess):
+        datum = op.Datum()
+        datum.cvInputData = imageToProcess
+        self.opWrapper.emplaceAndPop([datum])
+        return datum.poseKeypoints
---
+++ openpose/main.py
@@ -0,0 +1,9 @@
+import sys
+from detect_pose import PoseDetector
+import cv2
+
+if __name__=='__main__':
+    poseDetector = PoseDetector()
+    img = cv2.imread(sys.argv[1])
+    keypoints = poseDetector.processImage(img)
+    print(keypoints)
