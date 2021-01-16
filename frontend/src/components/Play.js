73a623d53317462e535b27b1057b38d3bd787799
zwei22 <wei851010@gmail.com>
2021-01-16 17:32:21 +0800

    add frontend structure

---
+++ frontend/src/components/Play.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+
+
+const Play = () =>{
+    return (
+        <>
+        </>
+    )
+}
+
+export default Play
---
+++ frontend/src/components/Setting.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+
+
+const Setting = () =>{
+    return (
+        <>
+        </>
+    )
+}
+
+export default Setting
