d903db04b9494cc8eeb5c163d2fb6c5ebf5db050
zwei22 <wei851010@gmail.com>
2021-01-18 22:32:59 +0800

    add webcam

--- frontend/src/components/PlayGame.js
+++ frontend/src/components/PlayGame.js
@@ -1,13 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import Webcam from "react-webcam";
+import JoJoText from "./JoJoText";
 
-const PlayGame = () =>{
-    return (
-        <>
-        PlayGame
-        {/* <Webcam /> */}
-        </>
-    )
-}
+const PlayGame = () => {
+  const webcamRef = useRef(null);
+  const [imgSrc, setImgSrc] = useState(null);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+  }, [webcamRef, setImgSrc]);
+  return (
+    <div className="round-border main-box">
+        <JoJoText>Click on video for screenshot</JoJoText>
+      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" onClick={capture} style={{width:"40%"}}/>
+      {imgSrc && <img src={imgSrc} style={{width:"40%", height:"auto"}}/>}
+    </div>
+  );
+};
 
-export default PlayGame
+export default PlayGame;
