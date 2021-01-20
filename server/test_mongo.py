69681c8f32ddad1a55b048d60c9f0cf91dac33bd
tzyLee <b06901145@ntu.edu.tw>
2021-01-20 20:57:50 +0800

    Update thumbnail

--- server/test_mongo.py
+++ server/test_mongo.py
@@ -2,13 +2,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-import bcrypt
 from core import mongodb
 
-password = b"pwd4zwei"
-hashed = bcrypt.hashpw(password, bcrypt.gensalt(14))
-print(bcrypt.checkpw(password, hashed))
 # This file is used to test mongoengine
 models = mongodb.models
 
-models.User(username="zwei", hashedPassword=hashed, inventory=[{"name":"zawarodo","quantity":22}], money=1e3).save()
+with open('temp', 'r') as f:
+    stage = models.Stage.objects(difficulty=5).first()
+    ret = stage.update(set__thumbnail=f.read())
+    print(ret)
