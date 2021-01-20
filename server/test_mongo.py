32700f96e0abb5b593915b7bc506f28d4f34c318
tzyLee <b06901145@ntu.edu.tw>
2021-01-21 04:20:09 +0800

    Update answer

--- server/test_mongo.py
+++ server/test_mongo.py
@@ -7,7 +7,14 @@ from core import mongodb
 # This file is used to test mongoengine
 models = mongodb.models
 
-with open('temp', 'r') as f:
-    stage = models.Stage.objects(difficulty=5).first()
-    ret = stage.update(set__thumbnail=f.read())
-    print(ret)
+
+from os import listdir
+from os.path import join, dirname
+
+root = join(dirname(__file__), "..", "..", "drive-download-20210120T135410Z-001")
+for stageID in listdir(root):
+    subdir = join(root, stageID)
+    for imgID in listdir(subdir):
+        with open(join(subdir, imgID, "answer"), "r") as f:
+            ans = f.read()
+        print(models.Image.objects(id=imgID).update(set__answer=ans))
