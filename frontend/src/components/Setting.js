246f8bb17f1bf368817a034c2fb80346966090eb
zwei22 <wei851010@gmail.com>
2021-01-20 21:47:35 +0800

    Add audio & volume setting

--- frontend/src/components/Setting.js
+++ frontend/src/components/Setting.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { Slider, Switch } from "antd";
 import JoJoText from "./JoJoText";
 
-const Setting = ({ onToMenu }) => {
+const Setting = ({ onToMenu, volume, setVolume }) => {
   return (
     <>
-      <div className="button">
-        <JoJoText style={{ fontSize: "35px" }} onClick={onToMenu}>
-          back
+      <div className="round-border main-box">
+        <div className="row-flex">
+
+        <JoJoText style={{ fontSize: "20px" }} onClick={onToMenu}>
+          BGM Volume
         </JoJoText>
+        <Slider value={volume} onChange={(v) => setVolume(v)} style={{width:"20%"}}/>
+        </div>
+        <div className="button">
+          <JoJoText style={{ fontSize: "35px" }} onClick={onToMenu}>
+            back
+          </JoJoText>
+        </div>
       </div>
     </>
   );
